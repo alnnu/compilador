3fd68777eae22adf71633de4826d97293abb22d4
alnnu <launnf1020@gmail.com>
2025-07-04 11:45:37 -0300

    refactoring code

--- src/lexico.c
+++ src/lexico.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -42,50 +43,74 @@ int prefix(const char *pre, const char *str) {
   return returnValue;
 
 }
-int verifySymbols(char line[256], int lineNumber) {
-  char *pch;
+
+
+int is_separator(char c) {
+    return c == ' ' || c == ',' || c == '(' || c == ')' || 
+           c == ';' || c == '{' || c == '}' || c == '\n' || c == '\0';
+}
+
+int isVaribleOrFunction(char *token) {
+  if (prefix("__", token) == 0 || prefix("!", token) == 0) {
+    return 0; 
+  }else {
+    return -1;
+  }
+}
+
+int isReservedWord(char *token) {
   int i = 0;
-  int errorFlag = 0;  
-  pch = strtok(line, " (){};,\n\0");
-
-  while (pch != NULL) {
-    i = 0;
-    printf("pch: %s\n", pch);
-    if(strcmp(pch, " ") != 0 && strcmp(pch, "\n") != 0) {
-      if (prefix("__", pch) != 0 && prefix("!", pch) != 0) {
-        for(i; i < 11; i++) {
-          if(strcmp(pch, reserveWords[i]) == 0) {
-            errorFlag = 0;
-            break;
-          }else {
-            errorFlag = 1;
-          }
-        }
-        
-        if (errorFlag == 0) {
-          pch = strtok(NULL, " (){};,\n\0");
-          continue;
-        }
-
-        for(i = 0; i < 14; i++) {
-          if(strcmp(pch, operators[i]) == 0) {
-            errorFlag = 0;
-            break;
-          }else {
-            errorFlag = 1;
-          }
-        }
-      }
+  for(i = 0; i < 11; i++) {
+    if(strcmp(token, reserveWords[i]) == 0) {
+      return 0; 
     }
-    if (errorFlag == 1) {
-      printf("Erro na linha %d, na palavra '%s' \n", lineNumber, pch);
-      break;
-    } 
-    pch = strtok (NULL, " (){};,\n\0");
+  }
+  return -1; 
+}
+
+int isOperator(char *token) {
+  int i = 0;
+  for(i = 0; i < 14; i++) {
+    if(strcmp(token, operators[i]) == 0) {
+      return 0;   
+    }
+  }
+  return -1;
+}
+
+int verifySymbols(char line[256], int listNumberd) {
+  int i = 0;
+  int j = 0;
+
+  char *tokens;
+
+  int errorFlag = 0;
+
+  tokens = (char *)malloc(256 * sizeof(char));
+
+  
+  
+  while (line[i] != '\0') {
+    if (is_separator(line[i]) == 0) {
+      j = 0;
+
+      while(is_separator(line[i]) == 0 && line[i] != '\0') {
+        tokens[j++] = line[i++];
+      }
+      tokens[j] = '\0';
+      printf("Token: %s\n", tokens);
+      if(isReservedWord(tokens) != 0 && isVaribleOrFunction(tokens) != 0 && isOperator(tokens) != 0) {
+        errorFlag = 1;
+        printf("Erro na linha %d, na palavra '%s'\n", listNumberd, tokens);
+        break;
+      }
+    }else {
+      i++;
+    }
   }
 
   return errorFlag;
-
 }
+
 
 
